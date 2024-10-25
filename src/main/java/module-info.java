a92e8074e08a9e7d1f5e3aa3b5884bffba6c7397
haihai <truongvanhai868@gmail.com>
2024-10-25 19:29:05 +0700

    check

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,9 +10,12 @@ module org.example.btl {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
-    requires java.sql;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.naming;
 
-    opens org.example.btl to javafx.fxml;
+    opens org.example.btl to javafx.fxml, org.hibernate.orm.core;
     opens org.example.btl.controllers to javafx.fxml;
+    opens org.example.btl.libraryManage to org.hibernate.orm.core;
     exports org.example.btl;
 }
--- src/main/java/org/example/btl/App.java
+++ src/main/java/org/example/btl/App.java
@@ -24,6 +24,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }

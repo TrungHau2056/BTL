9c1ed2e29427ffd5f4c6eb5ceb40e2cd2836b960
haihai <truongvanhai868@gmail.com>
2024-10-30 15:33:19 +0700

    check

--- src/main/java/org/example/btl/controllers/SignUpController.java
+++ src/main/java/org/example/btl/controllers/SignUpController.java
@@ -1,18 +1,47 @@
 package org.example.btl.controllers;
 
+import jakarta.persistence.Query;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import org.example.btl.libraryManage.Account;
+import org.example.btl.libraryManage.Admin;
+import org.example.btl.libraryManage.HibernateUtils;
+import org.hibernate.Session;
 
+import javax.lang.model.element.AnnotationMirror;
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
 
 public class SignUpController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    private TextField nameText;
+    @FXML
+    private TextField emailText;
+    @FXML
+    private TextField usernameText;
+    @FXML
+    private PasswordField passwordText;
+    @FXML
+    private PasswordField confirmedPasswordText;
+    @FXML
+    private DatePicker birthdayText;
+    @FXML
+    private ToggleGroup gender;
+
 
     public void switchToLogin(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/btl/loginScene.fxml"));
@@ -21,5 +50,64 @@ public class SignUpController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void handleSignUp(ActionEvent event) {
+        String name = nameText.getText();
+        String email = emailText.getText();
+        String username = usernameText.getText();
+        String password = passwordText.getText();
+        String confirmedPassword = confirmedPasswordText.getText();
+
+        RadioButton selectedGender = (RadioButton) gender.getSelectedToggle();
+        String gender = selectedGender == null ? "" : selectedGender.getText();
+
+        Date birthday = birthdayText.getValue() == null ? null : Date.valueOf(birthdayText.getValue());
+
+        if (Objects.equals(name, "")
+            || Objects.equals(email, "")
+            || Objects.equals(username, "")
+            || Objects.equals(password, "")
+            || Objects.equals(confirmedPassword, "")
+            || Objects.equals(gender, "")
+            || Objects.equals(birthday, null)) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please enter all the information!");
+            alert.show();
+        } else if (!Objects.equals(password, confirmedPassword)) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Passwords do not match. Please try again.");
+            alert.show();
+        } else if (username.length() < 6) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Your username must be at least 6 characters long.");
+            alert.show();
+        } else if (password.length() < 8) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Your Password must be at least 8 characters long.");
+            alert.show();
+        } else {
+            Session session = HibernateUtils.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query query = session.createQuery("FROM Admin WHERE username = :username");
+            query.setParameter("username", username);
+            List admins = query.getResultList();
+            session.close();
+            HibernateUtils.shutdown();
+            if (admins != null) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("This username already exists. Please choose another one.");
+                alert.show();
+            } else {
+                session.beginTransaction();
+                Account admin = new Admin(name, email, username, password, birthday, gender);
+                session.persist(admin);
+                session.getTransaction().commit();
+                session.close();
+                HibernateUtils.shutdown();
+            }
+            //remember to change scene here
+        }
+
     }
 }
--- src/main/java/org/example/btl/libraryManage/Admin.java
+++ src/main/java/org/example/btl/libraryManage/Admin.java
@@ -1,5 +1,34 @@
 package org.example.btl.libraryManage;
 
-public class Admin extends Account {
+import jakarta.persistence.*;
 
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Admin extends Account {
+    @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL)
+    private Set<Document> addedDocuments = new HashSet<>();
+
+    public Admin() {
+
+    }
+
+    public Admin(String name, String email, String username, String password, Date birthday, String gender) {
+        super(name, email, username, password, birthday, gender);
+    }
+
+    public Set<Document> getDocuments() {
+        return addedDocuments;
+    }
+
+    public void setDocuments(Set<Document> documents) {
+        this.addedDocuments = documents;
+    }
+
+    public void addDocument(Document document) {
+        document.setAdmin(this);
+        addedDocuments.add(document);
+    }
 }

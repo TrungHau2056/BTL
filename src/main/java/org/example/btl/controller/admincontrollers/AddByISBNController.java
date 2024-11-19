d914d2ea89f0df4cb3955a3aacd63cf81fbde217
haihai <truongvanhai868@gmail.com>
2024-11-19 14:33:05 +0700

    addbook author + genre

--- src/main/java/org/example/btl/controller/admincontrollers/AddByISBNController.java
+++ src/main/java/org/example/btl/controller/admincontrollers/AddByISBNController.java
@@ -1,8 +1,32 @@
 package org.example.btl.controller.admincontrollers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class AddByISBNController extends AdminBaseController {
+    @FXML
+    TextField isbnText;
+    @FXML
+    TextField titleText;
+    @FXML
+    TextField quantityText;
+    @FXML
+    TextField genreText;
+    @FXML
+    TextArea descriptionText;
+    @FXML
+    TextField authorText;
+    @FXML
+    TextField publisherText;
+
     @Override
     public void setAdminInfo() {
 
+    }
+
+    public void handleCheck(ActionEvent event) {
+        String isbn = isbnText.getText();
     }
 }

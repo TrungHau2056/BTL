d3580b68ab61dc89a2fef5bfa295c02f83ea51e4
dinhuet <23020043@vnu.edu.vn>
2024-11-24 11:34:15 +0700

    add notifications

---
+++ src/main/java/org/example/btl/controller/usercontrollers/UserNotificationController.java
@@ -0,0 +1,18 @@
+package org.example.btl.controller.usercontrollers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserNotificationController extends UserBaseController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @Override
+    public void setUserInfo() {
+
+    }
+}

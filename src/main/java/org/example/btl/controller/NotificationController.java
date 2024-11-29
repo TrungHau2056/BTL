2251abc846228ccd5bdd8dd21d329e46045a6e86
haihai <truongvanhai868@gmail.com>
2024-11-29 17:44:12 +0700

    unread, all notification

--- src/main/java/org/example/btl/controller/NotificationController.java
+++ src/main/java/org/example/btl/controller/NotificationController.java
@@ -78,8 +78,4 @@ public class NotificationController {
         Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         stage.close();
     }
-
-    public void handleDeleteAll() {
-        user = notificationService.deleteAllNoti(user);
-    }
 }
